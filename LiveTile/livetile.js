209ff1989b0b5607826e5afa7d6f2fdb705c3eeb
Noah Addy <v-naddy@hotmail.com>
2012-12-07 17:45:14 -0800

    adding prototype stuff

---
+++ LiveTile/livetile.js
@@ -0,0 +1,27 @@
+﻿/// <reference path="//Microsoft.WinJS.1.0.RC/js/base.js" />
+/// <reference path="//Microsoft.WinJS.1.0.RC/js/ui.js" />
+
+(function () {
+    'use strict';
+
+    $(document).ready(function () {
+        $(".livetile").addClass("animate");
+
+        //setInterval(function () {
+        //    $(".wrapper").removeClass("animate");
+        //    $(".wrapper").addClass("animate");
+
+        //}, 8000);
+        //setTimeout(function () {
+
+
+        //}, 4000);
+        
+        //document.querySelector(".wrapper").addEventListener("animationend", function () {
+        //    //setTimeout(function() {
+
+
+        //    //}, 4000);
+        //});
+    });
+})();
